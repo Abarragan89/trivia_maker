95194c1e4a7b74e31870e5044bf777f7dd55d7e7
Anthony Barragan <anthony.bar.89@gmail.com>
2022-07-06 19:39:34 -0700

    Feature: questions appearing as a modal is fully functional and buttons disable once clicked

--- client/src/Components/Modal/modal.js
+++ client/src/Components/Modal/modal.js
@@ -1,39 +1,23 @@
 import './modal.css';
 import { FaCheck } from 'react-icons/fa';
 import { FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 function Modal({ questionData, onClose }) {
-    console.log(questionData)
     // remove points if user gets question wrong
     function removePoints(questionPoints) {
         console.log(500 - questionPoints);
-        onClose();
-
     }
-
-    // function disableButton(ListElId) {
-    //     console.log('disabling')
-    //     for (let i = 0; i < ListElId.length; i++) {
-    //         let listEl = document.getElementById(ListElId).setAttribute('class', 'disabledButton');
-    //         console.log(listEl)
-    //     }
-    //     // window.location.replace('/question')
-    // }
-    // disableButton(liEl)
 
     return (
         <div className='modalBackdrop'>
-        <section>
-            <div className='flex-box-sa mt-4'>
+        <section className='modalContainer'>
+            <div className='modalHeader flex-box-sb'>
                 <h4>Username</h4>
-                <button onClick={onClose}><FaTimes /></button>
+                <p id='exitModal' onClick={onClose}><FaTimes /></p>
             </div>
             <article className='questionContainer flex-box-col-sa'>
-                <div>
-                    <p className='cardQuestion'>{questionData.question}</p>
-                </div>
-                <div className='flex-box-sa'>
+                    <p className='cardQuestionText'>{questionData.question}</p>
+                <div className='flex-box-sb'>
                     <button onClick={() => removePoints(questionData.points)}><FaTimes className='grading-icon' /></button>
                     <button><FaCheck className='grading-icon' /></button>
                 </div>
