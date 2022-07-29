ec15810054d4033652be29224e7a07d82e6e0a5c
Anthony Barragan <anthony.bar.89@gmail.com>
2022-07-28 23:04:04 -0700

    Feature: Pagination is styled and fully functional

--- client/src/Components/Pagination/pagination.js
+++ client/src/Components/Pagination/pagination.js
@@ -1,20 +1,68 @@
 import './pagination.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import usePagination, { DOTS } from '../../utils/paginationHooks';
 
-function Pagination ({ gamesPerPage, totalGames, paginate}) {
-    const pageNumbers = [];
+function Pagination({
+    gamesPerPage,
+    totalGames,
+    paginate,
+    currentPage }) {
 
-    for (let i = 1; i <= Math.ceil(totalGames / gamesPerPage); i++)
-        pageNumbers.push(i);
+
+    const pageNumbers = usePagination(currentPage, totalGames, gamesPerPage)
+
+    const onNext = () => {
+        paginate(currentPage + 1);
+    };
+
+    const onPrevious = () => {
+        paginate(currentPage - 1);
+    };
+
+    // determine last page in relation to page size to test against current page to conditionally disable arrows
+    const lastPage = Math.ceil(totalGames / gamesPerPage)
+
     return (
         <div>
             <ul className='pagination-ul'>
-                {pageNumbers.map(number => (
-                    <li key={number} className='page-item'>
-                        <p onClick={() => paginate(number)} className='page-link'>
-                            {number}
-                        </p>
-                    </li>
-                ))}
+                <li>
+                    <button
+                        onClick={onPrevious}
+                        disabled={currentPage === 1 || lastPage === 1}
+                        className={`${currentPage === 1 || lastPage === 1 ? 'disabled-chevron' : ''} chevron chev-left`}>
+                        <FaChevronLeft />
+                    </button>
+                </li>
+                {pageNumbers.map((pageNumber, index) => {
+
+                    if (pageNumber === DOTS) {
+                        return (
+                            <li key={index} className="dots">
+                                &#8230;
+                            </li>
+                        );
+                    }
+
+                    return (
+                        <li
+                            key={index}>
+                            <button
+                                className={`${pageNumber === currentPage ? 'currentPaginate' : 'false'}`}
+                                type="button"
+                                onClick={() => paginate(pageNumber)}>
+                                {pageNumber}
+                            </button>
+                        </li>
+                    );
+                })}
+                <li>
+                    <button
+                        onClick={onNext}
+                        disabled={currentPage === lastPage || lastPage === 1}
+                        className={`${currentPage === lastPage || lastPage === 1 ? 'disabled-chevron' : ''} chevron chev-right`}>
+                        <FaChevronRight />
+                    </button>
+                </li>
             </ul>
         </div>
     )
