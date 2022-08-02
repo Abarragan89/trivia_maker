27d3dcd63ffe1b1df3928c01048dae5a10019fd5
Anthony Barragan <anthony.bar.89@gmail.com>
2022-08-02 02:23:54 -0700

    Feature: Back button on study set and input field records user input

--- client/src/Pages/StudentStudy/studentStudy.js
+++ client/src/Pages/StudentStudy/studentStudy.js
@@ -1,13 +1,26 @@
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_STUDY_SET } from '../../utils/queries';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header/header';
 import './studentStudy.css';
 import { useState } from 'react';
+import { AiOutlineArrowRight } from 'react-icons/ai'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import useSound from 'use-sound';
+import mouseClick from '../../assets/sounds/mouse-click.wav';
+import flipCard from '../../assets/sounds/flip-card.wav';
+import wrongAnswer from '../../assets/sounds/wrong-answer-flashcard.wav';
+
 
 function StudentStudy() {
-    const gameId = useParams().gameId
+    // MouseClick Sound
+    const [mouseClickSound] = useSound(mouseClick, { volume: .3 })
+    const [wrongAnswerSound] = useSound(wrongAnswer, { volume: .3 })
+    const [flipCardSound] = useSound(flipCard, { volume: .3 })
 
+
+    const gameId = useParams().gameId
+    const navigate = useNavigate();
     const { data } = useQuery(QUERY_SINGLE_STUDY_SET, {
         variables: { gameId }
     })
@@ -33,9 +46,10 @@ function StudentStudy() {
     const [flip, setFlip] = useState(false)
 
     function nextCard(number) {
+        mouseClickSound();
         setFlip(false)
         const allStudyCards = document.querySelectorAll('.study-card');
-        if(number > allStudyCards.length - 1) {
+        if (number > allStudyCards.length - 1) {
             return;
         }
         setCurrentCard(currentCard + 1);
@@ -49,9 +63,10 @@ function StudentStudy() {
         }
     }
     function previousCard(number) {
+        mouseClickSound();
         setFlip(false);
         const allStudyCards = document.querySelectorAll('.study-card')
-        if(number < 0) {
+        if (number < 0) {
             return;
         }
         setCurrentCard(currentCard - 1)
@@ -64,15 +79,37 @@ function StudentStudy() {
         }
     }
 
+    function flipCardFunct() {
+        flipCardSound();
+        setFlip(!flip)
+    }
+
+    // adding score function
+    const [score, setScore] = useState(0)
+
+    function scoreResponse(e, answer) {
+        e.preventDefault();
+        console.log(e.target.firstChild.value)
+        let response = e.target.firstChild.value;
+        if (response.toLowerCase() === answer.toLowerCase()){ 
+            setScore(score + 1)
+            flipCardFunct();
+        } else {
+            wrongAnswerSound()        }
+    }
+    // need to remove card once correct
+
     return (
         <>
             <Header />
             <main id='student-study'>
+                <button className='study-set-back-btn' onClick={() => navigate(-1)}>Back to Study Sets</button>
                 <h1>{studyTopic}</h1>
+                <h2>Score: <span>{score}</span></h2>
                 <section className='flex-box-sa card-line'>
                     {studySet.map((card, index) => (
                         <div
-                            onClick={() => setFlip(!flip)}
+                            onClick={flipCardFunct}
                             className={`study-card ${flip ? 'flip' : ''}`}
                             key={index}>
                             <div className='study-info card-front'>
@@ -85,9 +122,14 @@ function StudentStudy() {
                     ))}
                 </section>
                 <div className='flex-box-sa studyset-btns'>
-                    <button className='move-card' onClick={() => previousCard(currentCard - 1)}>Back</button>
+                    <button className='move-card' onClick={() => previousCard(currentCard - 1)}><AiOutlineArrowLeft /></button>
                     <p>{currentCard + 1} / {studySet.length}</p>
-                    <button className='move-card' onClick={() => nextCard(currentCard + 1)}>Next</button>
+                    <button className='move-card' onClick={() => nextCard(currentCard + 1)}><AiOutlineArrowRight /></button>
+                </div>
+                <div>
+                    <form onSubmit={(e) => scoreResponse(e, studySet[currentCard].answer)}>
+                        <input type='text' id='answer-flashcard' />
+                    </form>
                 </div>
             </main>
         </>
