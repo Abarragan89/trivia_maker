aa9ed3606508a8519bedfe2f053d94fb1a586ada
Anthony Barragan <anthonybarragan@MacBook-Pro-2.local>
2023-02-09 22:51:09 -0800

    Test

--- server/models/User.js
+++ server/models/User.js
@@ -49,7 +49,7 @@ userSchema.pre('save', async function(next) {
   
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function(password) {
-return bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password);
 };
 
 const User = model('user', userSchema);
